88719b3f1abc7e24025c8ddd58e73da5ea4090f1
micallef25 <30693668+micallef25@users.noreply.github.com>
2019-09-10 16:53:46 -0400

    Update naive.cu
    started naive scan ... need to check

--- Project2-Stream-Compaction/stream_compaction/naive.cu
+++ Project2-Stream-Compaction/stream_compaction/naive.cu
@@ -14,15 +14,24 @@ namespace StreamCompaction {
         // TODO: __global__
         
         // out put and two input buffers to ping pong off of
+        // 
         __global__ void kernel_scan( int n, int* odata, int* idata )
         {
-            if( tid >= n ) // already been computed so just copy over
+            if( tid >= n ) // already been computed
             {
-                odata = idata;
+                __syncthreads(); // need this or will lock ): 
+                odata[thid] = (tid == 0) ?  0 :  idata[tid];
+                return;
             }
             
-            idata[
-            
+            //the original way ... but this wont work because we have threads manipulating the neighbors
+            //odata[tid] = idata[tid-1] + odata[tid-1];
+            int prev = odata[tid-1];
+            // read your neighbors and wait
+            __syncthreads();
+            // now we can write as before.
+            odata[tid] = idata[tid-1] + prev;
+            return;
         }
 
         /**
@@ -31,14 +40,26 @@ namespace StreamCompaction {
         void scan(int n, int *odata, const int *idata) {
             timer().startGpuTimer();
             // TODO
-            
+            int* dev_temp_in;
+            int* dev_input;
             //create cuda buffers and copy data over
-            cudaalloc(temp_in);
-            cudaalloc(input);
-            cudacopy(idata,input);
+            cudaMalloc((void**)&dev_temp_in, n * sizeof(int));
+            cudaMalloc((void**)&dev_input, n * sizeof(int));
+            // copy data to device n or n*size? check
+            cudaCopy( dev_input, idata, n, cudaMemcpyHostToDevice );
+          
             
-            o_data[0] = 0;
+            // think this itr count needs to be changed
+            for(int i = 0; i < n; i++)
+            {
+                kernel_scan(n-i,dev_temp_in,dev_input);
+                std::swap(dev_temp_in,dev_input);
+            }
             
+            cudaCopy( odata, dev_input, n, cudaMemcpyDeviceToHost );
+                
+            cudaFree(dev_input);
+            cudaFree(dev_temp_in);
             
             timer().endGpuTimer();
         }
