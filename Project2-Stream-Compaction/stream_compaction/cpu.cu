588469bf0d631a3bf9ce058cf78c818a5db9b86b
Vaibhav Arcot <yvarcot@seas.upenn.edu>
2019-09-11 02:32:23 -0400

    Finished cpu version and naive version (stream) of stream_compaction subproblem

--- Project2-Stream-Compaction/stream_compaction/cpu.cu
+++ Project2-Stream-Compaction/stream_compaction/cpu.cu
@@ -1,6 +1,6 @@
 #include <cstdio>
 #include "cpu.h"
-
+#include <iostream>
 #include "common.h"
 
 namespace StreamCompaction {
@@ -17,10 +17,15 @@ namespace StreamCompaction {
          * For performance analysis, this is supposed to be a simple for loop.
          * (Optional) For better understanding before starting moving to GPU, you can simulate your GPU scan in this function first.
          */
-        void scan(int n, int *odata, const int *idata) {
-	        timer().startCpuTimer();
-            // TODO
-	        timer().endCpuTimer();
+        void scan(int n, int *odata, const int *idata, const bool time/* = true*/) {
+	        (time == true)?timer().startCpuTimer():0;
+			int running_total = 0;
+			for (int i = 0; i < n-1; i++) { // n-1 because we want exclusive not inclusive so last element isn't added
+				running_total += idata[i]; // n-1 adds for n size 
+				odata[i+1] = running_total;
+			}
+			(time == true) ? timer().endCpuTimer() : 0;
+			
         }
 
         /**
@@ -30,9 +35,15 @@ namespace StreamCompaction {
          */
         int compactWithoutScan(int n, int *odata, const int *idata) {
 	        timer().startCpuTimer();
-            // TODO
+			int count = 0;
+			int odata_pos = 0;
+			for (int i = 0; i < n; i++)
+				if (idata[i] != 0) {
+					count++;
+					odata[++odata_pos] = idata[i];
+				}
 	        timer().endCpuTimer();
-            return -1;
+            return count;
         }
 
         /**
@@ -42,9 +53,27 @@ namespace StreamCompaction {
          */
         int compactWithScan(int n, int *odata, const int *idata) {
 	        timer().startCpuTimer();
-	        // TODO
+	        // Step 1, mark each cell with 1/0 if it has a element or not (super easy to parallelize)
+			int* scan_data = new int[n]();
+			int* mask = new int[n]();
+			for (int i = 0; i < n; i++) {
+				if (idata[i])
+					mask[i] = 1;
+				else
+					mask[i] = 0;
+			}
+			// scan the mask array (can be done in parallel by using a balanced binary tree)
+			scan(n, scan_data, mask, false);
+			// Scatter array (go to each position and copy the value) (super easy to parallelize)
+			for (int i = 0; i < n - 1; i++) {
+				if (idata[i])
+					odata[scan_data[i] + 1] = idata[i]; 
+			}
+			int res = scan_data[n - 1];
+			delete scan_data;
+			delete mask;
 	        timer().endCpuTimer();
-            return -1;
+            return res;
         }
     }
 }
--- Project2-Stream-Compaction/stream_compaction/naive.cu
+++ Project2-Stream-Compaction/stream_compaction/naive.cu
@@ -2,6 +2,8 @@
 #include <cuda_runtime.h>
 #include "common.h"
 #include "naive.h"
+#include <iostream>
+#define checkCUDAErrorWithLine(msg) checkCUDAError(msg, __LINE__)
 
 namespace StreamCompaction {
     namespace Naive {
@@ -12,13 +14,49 @@ namespace StreamCompaction {
             return timer;
         }
         // TODO: __global__
-
+		__global__ void naive_parallel_scan(int n, int *odata, const int *idata, int d) {
+			int index = blockDim.x * blockIdx.x + threadIdx.x;
+			if (index > n)
+				return;
+			if (index >= powf(2,d-1))
+				odata[index] = idata[index - (int)powf(2,d - 1)] + idata[index]; // todo figure out why this doesnt work with non powers of 2
+			else
+				odata[index] = idata[index];
+		}
+		__global__ void right_shift(int n, int *odata, const int *idata) {
+			int index = blockDim.x * blockIdx.x + threadIdx.x;
+			if (index > n)
+				return;
+			if (index == 0)
+				odata[index] = 0;
+			else
+				odata[index] = idata[index - 1];
+		}
         /**
          * Performs prefix-sum (aka scan) on idata, storing the result into odata.
          */
         void scan(int n, int *odata, const int *idata) {
             timer().startGpuTimer();
-            // TODO
+			int blocksize = 128;
+			int blocks = (n + blocksize - 1) / blocksize;
+			// allocate data
+			int *dev_odata, *dev_odata_2;
+			cudaMalloc((void**)&dev_odata, n * sizeof(int));
+			cudaMalloc((void**)&dev_odata_2, n * sizeof(int));
+			// copy data over
+			cudaMemcpy(dev_odata, idata, n*sizeof(int), cudaMemcpyHostToDevice);
+			checkCUDAErrorWithLine("memcpy back failed!");
+			for (int d = 1; d <= ceil(log2(n)); d++) {
+				naive_parallel_scan <<<blocks, blocksize >> > (n, dev_odata_2, dev_odata, d);
+				checkCUDAErrorWithLine("copy fn failed!");
+				std::swap(dev_odata, dev_odata_2);
+			}
+			right_shift << <blocks, blocksize >> > (n, dev_odata_2, dev_odata);
+			cudaMemcpy(odata, dev_odata_2, n*sizeof(int), cudaMemcpyDeviceToHost);
+			checkCUDAErrorWithLine("memcpy back failed!");
+			cudaFree(dev_odata);
+			cudaFree(dev_odata_2);
+			// create buffer
             timer().endGpuTimer();
         }
     }
