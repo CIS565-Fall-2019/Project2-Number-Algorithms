47ae045241462ab8441d8ea43db02c74716e9bd3
DISHA JINDAL <djjindal@park.key.seas.upenn.edu>
2019-09-12 18:03:11 -0400

    cpu

--- Project2-Stream-Compaction/stream_compaction/cpu.cu
+++ Project2-Stream-Compaction/stream_compaction/cpu.cu
@@ -18,9 +18,19 @@ namespace StreamCompaction {
          * (Optional) For better understanding before starting moving to GPU, you can simulate your GPU scan in this function first.
          */
         void scan(int n, int *odata, const int *idata) {
-	        timer().startCpuTimer();
-            // TODO
-	        timer().endCpuTimer();
+			bool started_timer = true;
+			try {
+				timer().startCpuTimer();
+			} catch (const std::exception& e) {
+				started_timer = false;
+			}
+
+	        // TODO
+			odata[0] = 0;
+			for (int i = 1; i < n; i++)
+				odata[i] = odata[i - 1] + idata[i-1];
+			if(started_timer)
+		        timer().endCpuTimer();
         }
 
         /**
@@ -31,9 +41,25 @@ namespace StreamCompaction {
         int compactWithoutScan(int n, int *odata, const int *idata) {
 	        timer().startCpuTimer();
             // TODO
+			int j = 0;
+			for (int i = 0; i < n; i++)
+				if (idata[i] != 0)
+					odata[j++] = idata[i];
 	        timer().endCpuTimer();
-            return -1;
+            return j;
         }
+
+		void printArray(int n, int *a, bool abridged = false) {
+			printf("    [ ");
+			for (int i = 0; i < n; i++) {
+				if (abridged && i + 2 == 15 && n > 16) {
+					i = n - 2;
+					printf("... ");
+				}
+				printf("%3d ", a[i]);
+			}
+			printf("]\n");
+		}
 
         /**
          * CPU stream compaction using scan and scatter, like the parallel version.
@@ -43,8 +69,17 @@ namespace StreamCompaction {
         int compactWithScan(int n, int *odata, const int *idata) {
 	        timer().startCpuTimer();
 	        // TODO
+			int *binary_idata = new int[n];
+			int *final_index = new int[n];
+			for (int i = 0; i < n; i++)
+				binary_idata[i] = idata[i] > 0 ? 1 : 0;
+			scan(n, final_index, binary_idata);
+			for (int i = 0; i < n; i++)
+				if (binary_idata[i] == 1)
+					odata[final_index[i]] = idata[i];
 	        timer().endCpuTimer();
-            return -1;
+            return final_index[n-1] + (idata[n-1] > 0);
         }
     }
+	
 }
