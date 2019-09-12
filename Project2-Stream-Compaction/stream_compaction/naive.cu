5c4762523280a1d70fee1380ddf7029b1200be7a
SAKET KARVE <saketk@park.key.seas.upenn.edu>
2019-09-12 18:02:06 -0400

    CPU done.

--- Project2-Stream-Compaction/stream_compaction/naive.cu
+++ Project2-Stream-Compaction/stream_compaction/naive.cu
@@ -3,6 +3,13 @@
 #include "common.h"
 #include "naive.h"
 
+#define checkCUDAErrorWithLine(msg) checkCUDAError(msg, __LINE__)
+
+#define blockSize 128
+
+int* dev_A;
+int* dev_B;
+
 namespace StreamCompaction {
     namespace Naive {
         using StreamCompaction::Common::PerformanceTimer;
@@ -12,13 +19,34 @@ namespace StreamCompaction {
             return timer;
         }
         // TODO: __global__
+		__global__ void updateSum(int N, int d, int* A, int* B) {
+			int index = threadIdx.x + (blockIdx.x * blockDim.x);
+			if (index >= N) {
+				return;
+			}
+			if(index )
 
+		}
         /**
          * Performs prefix-sum (aka scan) on idata, storing the result into odata.
          */
         void scan(int n, int *odata, const int *idata) {
             timer().startGpuTimer();
-            // TODO
+			int iterations = ilog2ceil(n);
+
+			cudaMalloc((void**)&dev_A, n * sizeof(int));
+			checkCUDAErrorWithLine("cudaMalloc dev_A failed!");
+
+			cudaMalloc((void**)&dev_B, n * sizeof(int));
+			checkCUDAErrorWithLine("cudaMalloc dev_B failed!");
+
+
+
+
+			for (int d = 0; d < iterations; d++) {
+				dim3 fullBlocksPerGrid((n + blockSize - 1) / blockSize);
+
+			}
             timer().endGpuTimer();
         }
     }
