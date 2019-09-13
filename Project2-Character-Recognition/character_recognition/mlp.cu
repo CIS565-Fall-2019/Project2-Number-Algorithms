2d618bd838bd47aa20e22b57e72ef8677348bbbf
klaywittler <klaywittler@gmail.com>
2019-09-13 10:27:27 -0400

    some potential helper functions

--- Project2-Character-Recognition/character_recognition/mlp.cu
+++ Project2-Character-Recognition/character_recognition/mlp.cu
@@ -3,6 +3,9 @@
 #include "common.h"
 #include "mlp.h"
 
+/*! Block size used for CUDA kernel launch. */
+#define blockSize 128
+
 namespace CharacterRecognition {
     using Common::PerformanceTimer;
     PerformanceTimer& timer()
@@ -24,8 +27,62 @@ namespace CharacterRecognition {
     */
 
 	// TODO: implement required elements for MLP sections 1 and 2 here
+	__global__ void addVector(int n, float* vec1, float* vec2, float* result) {
+		int index = threadIdx.x + (blockIdx.x * blockDim.x);
+		if (index >= n) {
+			return;
+		}
+		result[index] = vec1[index] + vec2[index];
+	}
 
-	void train() {
+	__global__ void elementMultVect(int n, float *input, float *output, float *weight) {
+		int index = threadIdx.x + (blockIdx.x * blockDim.x);
+		if (index >= n) {
+			return;
+		}
+		output[index] = input[index]*weight[index];
+	}
+
+	float sigmoid(float x) {
+		return 1.0f / (1 + exp(-x));
+	}
+
+	float mlp(float *input, float *output, float *weight) {
+
+
+		//thrust::inclusive_scan(idata, idata + n, odata);
+	}
+
+	void network() {
 
 	}
+
+	void partition() {
+
+	}
+	void train(int n, float *input, float* output) {
+		int *dev_input;
+		int *dev_output;
+		int *dev_hidden;
+
+		cudaMalloc((void**)&dev_input, n * sizeof(int));
+		checkCUDAError("cudaMalloc dev_input failed!");
+
+		cudaMalloc((void**)&dev_hidden, n * sizeof(int));
+		checkCUDAError("cudaMalloc dev_output failed!");
+
+		cudaMalloc((void**)&dev_output, n * sizeof(int));
+		checkCUDAError("cudaMalloc dev_output failed!");
+
+		cudaMemcpy(dev_input, input, sizeof(int) * n, cudaMemcpyHostToDevice);
+		checkCUDAError("Memcpy input failed!");
+
+		dim3 threadsPerBlock(blockSize);
+		dim3 fullBlocksPerGrid((n + blockSize - 1) / blockSize);
+
+	}
+	void test() {
+
+	}
+
 }
--- Project2-Character-Recognition/character_recognition/mlp.h
+++ Project2-Character-Recognition/character_recognition/mlp.h
@@ -6,5 +6,7 @@ namespace CharacterRecognition {
     Common::PerformanceTimer& timer();
 
     // TODO: implement required elements for MLP sections 1 and 2 here
+	void partition();
 	void train();
+	void test();
 }
