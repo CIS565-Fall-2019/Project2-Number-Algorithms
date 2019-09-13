e5db042ac35de58e9942b953b9e9dfda92071bd4
DISHA JINDAL <djjindal@park.key.seas.upenn.edu>
2019-09-12 23:50:01 -0400

    Naive, Efficient and Thrust scans

--- Project2-Stream-Compaction/stream_compaction/efficient.cu
+++ Project2-Stream-Compaction/stream_compaction/efficient.cu
@@ -1,7 +1,9 @@
 #include <cuda.h>
 #include <cuda_runtime.h>
 #include "common.h"
+#include "device_launch_parameters.h"
 #include "efficient.h"
+#define blockSize 128
 
 namespace StreamCompaction {
     namespace Efficient {
@@ -12,13 +14,76 @@ namespace StreamCompaction {
             return timer;
         }
 
+		__global__ void kernelUpSweepStep(int n, int d, int* cdata) {
+			int k = (blockIdx.x * blockDim.x) + threadIdx.x;
+			if (k > n)
+				return;
+			int cur_step_size = 1 << (d + 1);
+			int prev_step_size = 1 << d;
+			if (k % cur_step_size == 0)
+				cdata[k + cur_step_size - 1] += cdata[k + prev_step_size - 1];
+		}
+
+		__global__ void kernelDownSweepStep(int n, int d, int* cdata) {
+			int k = (blockIdx.x * blockDim.x) + threadIdx.x;
+			if (k > n)
+				return;
+			int cur_step = 1 << (d + 1);
+			int left_step = 1 << d;
+
+			if (k % cur_step == 0) {
+				int temp = cdata[k + left_step - 1];
+				cdata[k + left_step - 1] = cdata[k + cur_step - 1];
+				cdata[k + cur_step - 1] += temp;
+			}
+		}
+		void printArray(int n, int *a, bool abridged = false) {
+			printf("    [ ");
+			for (int i = 0; i < n; i++) {
+				if (abridged && i + 2 == 15 && n > 16) {
+					i = n - 2;
+					printf("... ");
+				}
+				printf("%3d ", a[i]);
+			}
+			printf("]\n");
+		}
+
+		void printCudaArray(int size, int* cdata) {
+			int *d_data = new int[size];
+			cudaMemcpy(d_data, cdata, size * sizeof(int), cudaMemcpyDeviceToHost);
+			printArray(size, d_data, true);
+		}
+
         /**
          * Performs prefix-sum (aka scan) on idata, storing the result into odata.
          */
         void scan(int n, int *odata, const int *idata) {
             timer().startGpuTimer();
             // TODO
-            timer().endGpuTimer();
+			int power_size = pow(2, ilog2ceil(n));
+			int *cdata;
+			cudaMalloc((void**)&cdata, power_size * sizeof(int));
+			checkCUDAErrorFn("cudaMalloc adata failed!");
+
+			cudaMemset(cdata, 0, power_size * sizeof(int));
+			cudaMemcpy(cdata, idata, n * sizeof(int), cudaMemcpyHostToDevice);
+			dim3 fullBlocksPerGrid((power_size + blockSize - 1) / blockSize);
+
+			//Up Sweep
+			for (int d = 0; d < ilog2ceil(power_size) ; d++) {
+				kernelUpSweepStep <<<fullBlocksPerGrid, blockSize >>> (power_size, d, cdata);
+			}
+
+			//Down Sweep
+			cudaMemset(cdata + power_size - 1, 0, sizeof(int));
+
+			for (int d = ilog2(power_size) - 1; d >= 0; d--) {
+				kernelDownSweepStep << <fullBlocksPerGrid, blockSize >> > (power_size, d, cdata);
+			}
+			cudaMemcpy(odata, cdata, sizeof(int) * power_size, cudaMemcpyDeviceToHost);
+			cudaFree(cdata);
+			timer().endGpuTimer();
         }
 
         /**
@@ -33,6 +98,7 @@ namespace StreamCompaction {
         int compact(int n, int *odata, const int *idata) {
             timer().startGpuTimer();
             // TODO
+			
             timer().endGpuTimer();
             return -1;
         }
--- Project2-Stream-Compaction/stream_compaction/naive.cu
+++ Project2-Stream-Compaction/stream_compaction/naive.cu
@@ -2,24 +2,56 @@
 #include <cuda_runtime.h>
 #include "common.h"
 #include "naive.h"
+#include "device_launch_parameters.h"
+
+#define blockSize 128
 
 namespace StreamCompaction {
-    namespace Naive {
-        using StreamCompaction::Common::PerformanceTimer;
-        PerformanceTimer& timer()
-        {
-            static PerformanceTimer timer;
-            return timer;
-        }
-        // TODO: __global__
+	namespace Naive {
+		using StreamCompaction::Common::PerformanceTimer;
+		PerformanceTimer& timer()
+		{
+			static PerformanceTimer timer;
+			return timer;
+		}
+		// TODO: __global__
+		__global__ void kernelUpdateStep(int n, int d, int* dest_data, int* src_data) {
+			int k = (blockIdx.x * blockDim.x) + threadIdx.x;
+			if (k > n)
+				return;
+			int offset = 1 << (d - 1);
+			if (k >= offset)
+				dest_data[k] = src_data[k] + src_data[k - offset];
+			else
+				dest_data[k] = src_data[k];
 
-        /**
-         * Performs prefix-sum (aka scan) on idata, storing the result into odata.
-         */
-        void scan(int n, int *odata, const int *idata) {
-            timer().startGpuTimer();
-            // TODO
-            timer().endGpuTimer();
-        }
-    }
+		}
+		
+		/**
+		 * Performs prefix-sum (aka scan) on idata, storing the result into odata.
+		 */
+		void scan(int n, int *odata, const int *idata) {
+			timer().startGpuTimer();
+			// TODO
+			int *adata;
+			int *bdata;
+			cudaMalloc((void**)&adata, n * sizeof(int));
+			checkCUDAErrorFn("cudaMalloc adata failed!");
+			cudaMalloc((void**)&bdata, n * sizeof(int));
+			checkCUDAErrorFn("cudaMalloc bdata failed!");
+			cudaMemcpy(adata, idata, sizeof(int) * n, cudaMemcpyHostToDevice);
+
+			dim3 fullBlocksPerGrid((n + blockSize - 1) / blockSize);
+			for (int d = 1; d <= ilog2ceil(n); d++) {
+				kernelUpdateStep <<<fullBlocksPerGrid, blockSize >>> (n, d, bdata, adata);
+				std::swap(adata, bdata);
+				//cudaMemcpy(adata, bdata, sizeof(int) * n, cudaMemcpyDeviceToDevice); // Add this comparison 
+			}
+			cudaMemcpy(odata + 1, adata, sizeof(int) * (n - 1), cudaMemcpyDeviceToHost);
+			odata[0] = 0;
+			cudaFree(adata);
+			cudaFree(bdata);
+			timer().endGpuTimer();
+		}
+	}
 }
