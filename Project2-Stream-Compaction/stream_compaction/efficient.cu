fce71261e78af215f9fef8a68510e08344d748af
John Marcao <jmarcao@users.noreply.github.com>
2019-09-13 16:00:41 -0400

    Implemented Efficient Scan and Thrust Scan

--- Project2-Stream-Compaction/stream_compaction/efficient.cu
+++ Project2-Stream-Compaction/stream_compaction/efficient.cu
@@ -1,5 +1,7 @@
 #include <cuda.h>
 #include <cuda_runtime.h>
+#include <device_launch_parameters.h>
+#include <bitset>
 #include "common.h"
 #include "efficient.h"
 
@@ -12,13 +14,76 @@ namespace StreamCompaction {
             return timer;
         }
 
+		int nextPowerOfTwo(int in) {
+			int out = 0;
+			float log = log2(in);
+
+			// If this is true, the number IS a power of 2
+			if (ceil(log) == floor(log)) {
+				out = in;
+			}
+			else {
+				// Not a power of two, grab the next one up.
+				out = 1;
+				do {
+					out = out << 1;
+				} while (out < in);
+			}
+
+			return out;
+		}
+
         /**
          * Performs prefix-sum (aka scan) on idata, storing the result into odata.
          */
         void scan(int n, int *odata, const int *idata) {
-            timer().startGpuTimer();
-            // TODO
-            timer().endGpuTimer();
+			// Efficient algorithm uses balanded binary trees and two phases: upsweep and downsweep.
+			// This can be performed inplace.
+
+			// 0) Correct length to be Power of 2
+			const int N = nextPowerOfTwo(n); // Returns 'n' if input is already a power of 2.
+
+			// 1) Initialize Memory
+			int* dev_data = 0;
+			cudaMalloc(&dev_data, N * sizeof(int));
+			cudaMemset(dev_data + n, 0, (N - n) * sizeof(int));
+			checkCUDAError("CUDA memset 1 failed");
+			cudaMemcpy(dev_data, idata, n * sizeof(int), ::cudaMemcpyHostToDevice);
+
+			// 2) Upsweep
+			timer().startGpuTimer();
+			int* INSPECT = (int*)malloc(N * sizeof(int));
+			for (int d = 0; d <= ilog2ceil(N) - 1; d++) {
+				const int NUM_THREADS = N;
+				const int NUM_BLOCKS = 1;
+
+				// Kernel Call
+				cudaMemcpy(INSPECT, dev_data, N * sizeof(int), ::cudaMemcpyDeviceToHost);
+				kernWorkEffScanUpsweep<<<NUM_BLOCKS, NUM_THREADS>>>(n, d, dev_data, dev_data);
+				cudaMemcpy(INSPECT, dev_data, N * sizeof(int), ::cudaMemcpyDeviceToHost);
+				cudaDeviceSynchronize();
+			}
+
+			// 3) Downsweep
+			cudaMemset(dev_data + (N-1), 0, 1*sizeof(int));
+			checkCUDAError("CuDA memset 2 failed");
+			for (int d = ilog2ceil(N) - 1; d >= 0; d--) {
+				const int NUM_THREADS = N;
+				const int NUM_BLOCKS = 1;
+
+				// Kernel Call
+				cudaMemcpy(INSPECT, dev_data, N * sizeof(int), ::cudaMemcpyDeviceToHost);
+				kernWorkEffScanDownsweep << <NUM_BLOCKS, NUM_THREADS >> > (n, d, dev_data, dev_data);
+				cudaMemcpy(INSPECT, dev_data, N * sizeof(int), ::cudaMemcpyDeviceToHost);
+				cudaDeviceSynchronize();
+			}
+			
+			// 4) Cleanup
+			timer().endGpuTimer();
+			cudaMemcpy(odata, dev_data, N * sizeof(int), ::cudaMemcpyDeviceToHost);
+			cudaFree(dev_data);
+
+			return;
         }
 
         /**
@@ -36,5 +101,29 @@ namespace StreamCompaction {
             timer().endGpuTimer();
             return -1;
         }
+
+		__global__ void kernWorkEffScanUpsweep(const int N, const int D, int *out, const int* in) {
+			int k = threadIdx.x + (blockIdx.x * blockDim.x);
+			if (k >= N) {
+				return;
+			}
+
+			if (k % (int)powf(2, D + 1) == 0) {
+				out[k + (int)powf(2, D + 1) - 1] += in[k + (int)powf(2, D) - 1];
+			}
+		}
+
+		__global__ void kernWorkEffScanDownsweep(const int N, const int D, int *out, const int* in) {
+			int k = threadIdx.x + (blockIdx.x * blockDim.x);
+			if (k >= N) {
+				return;
+			}
+
+			if (k % (int)powf(2, D + 1) == 0) {
+				int tmp = in[k + (int)powf(2, D) - 1];
+				out[k + (int)powf(2, D) - 1] = out[k + (int)powf(2, D + 1) - 1];
+				out[k + (int)powf(2, D + 1) - 1] += tmp;
+			}
+		}
     }
 }
