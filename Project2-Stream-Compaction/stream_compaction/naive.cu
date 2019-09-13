e3e5a47adc64330912afb99245b2d00e3d067181
PEYMAN NOROUZI <pnorouzi@park.key.seas.upenn.edu>
2019-09-13 16:49:46 -0400

    Small Changes

--- Project2-Stream-Compaction/stream_compaction/naive.cu
+++ Project2-Stream-Compaction/stream_compaction/naive.cu
@@ -11,13 +11,30 @@ namespace StreamCompaction {
             static PerformanceTimer timer;
             return timer;
         }
+		int *dev_idata;
+		int *dev_odata;
+
         // TODO: __global__
+
+		__global__ void scan_GPU(int N, int *Dev_idata, int *Dev_odata) {
+
+		}
 
         /**
          * Performs prefix-sum (aka scan) on idata, storing the result into odata.
          */
         void scan(int n, int *odata, const int *idata) {
             timer().startGpuTimer();
+			int blockSize = 32;
+			cudaMalloc((void**)&dev_idata, n * sizeof(int));
+			cudaMemcpy(dev_idata, idata, sizeof(int) * n, cudaMemcpyHostToDevice);
+
+			cudaMalloc((void**)&dev_odata, n * sizeof(int));
+			cudaMemcpy(dev_odata, odata, sizeof(int) * n, cudaMemcpyHostToDevice);
+
+			dim3 fullBlocksPerGrid((n + blockSize - 1) / blockSize);
+
+			scan_GPU << <fullBlocksPerGrid, blockSize >> > (n, dev_idata, dev_odata);
             // TODO
             timer().endGpuTimer();
         }
