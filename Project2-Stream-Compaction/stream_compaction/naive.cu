1f7ff943248830272b22fd8e725ef1832fe87f10
Grace Gilbert <gracegi@seas.upenn.edu>
2019-09-12 23:22:37 -0500

    completed naive scan

--- Project2-Stream-Compaction/stream_compaction/naive.cu
+++ Project2-Stream-Compaction/stream_compaction/naive.cu
@@ -16,6 +16,31 @@ namespace StreamCompaction {
 		dim3 threadsPerBlock(blockSize);
 
 		__global__ void kernSumPairs(int N, int d, int *srcArray, int *dstArray) {
+			int index = (blockIdx.x * blockDim.x) + threadIdx.x;
+			if (index >= N) {
+				return;
+			}
+			int power = powf(2, d - 1);
+			index += power;
+			dstArray[index] = srcArray[index - power] + srcArray[index];
+
+			/*int power = powf(2, d - 1);
+			if (index >= power) {
+				dstArray[index] = srcArray[index - power] + srcArray[index];
+			}*/
+		}
+
+		__global__ void kernShift(int N, int *srcArray, int *dstArray) {
+			int index = (blockIdx.x * blockDim.x) + threadIdx.x;
+			if (index >= N) {
+				return;
+			}
+
+			if (index == 0) {
+				dstArray[index] = 0;
+				return;
+			}
+			dstArray[index] = srcArray[index - 1];
 
 		}
 
@@ -42,29 +67,41 @@ namespace StreamCompaction {
 			timer().startGpuTimer();
 
             // TODO
-			int alternator = 0;
-			for (int d = 0; d < ilog2ceil(n); ++d) {
-				int numThreads = pow(2, d);
+			bool alternator = true;
+			for (int d = 1; d <= ilog2ceil(n); ++d) {
+				int numThreads = bufferLength - pow(2, d - 1); // TODO: can this be smaller?
+				
 				dim3 blocksPerGrid((numThreads + blockSize - 1) / blockSize);
 
-				if (alternator % 2 == 0) {
+				if (alternator) {
 					kernSumPairs<<<blocksPerGrid, threadsPerBlock>>>(numThreads, d, dev_arrayA, dev_arrayB);
+					checkCUDAError("kernSumPairs failed!");
+
 					cudaMemcpy(dev_arrayA, dev_arrayB, bufferLength * sizeof(int), cudaMemcpyDeviceToDevice);
-					alternator++;
+					alternator = false;
 
 				}
 				else {
 					kernSumPairs<<<blocksPerGrid, threadsPerBlock>>>(numThreads, d, dev_arrayB, dev_arrayA);
-					cudaMemcpy(dev_arrayA, dev_arrayB, bufferLength * sizeof(int), cudaMemcpyDeviceToDevice);
-					alternator++;
+					checkCUDAError("kernSumPairs failed!");
+
+					cudaMemcpy(dev_arrayB, dev_arrayA, bufferLength * sizeof(int), cudaMemcpyDeviceToDevice);
+					alternator = true;
 
 				}
-
 			}
+			// Note: dev_arrayA/B are now inclusive scans.  
+			// We will take B and shift it on the gpu, storing the exlusive scan in A
 
+			int numThreads = bufferLength;
+			dim3 blocksPerGrid((numThreads + blockSize - 1) / blockSize);
+
+			kernShift<<<blocksPerGrid, threadsPerBlock>>>(bufferLength * sizeof(int), dev_arrayB, dev_arrayA);
+			checkCUDAError("kernShift failed!");
+
+			cudaMemcpy(odata, dev_arrayA, n * sizeof(int), cudaMemcpyDeviceToHost);
 
             timer().endGpuTimer();
-
 
 			cudaFree(dev_arrayA);
 			cudaFree(dev_arrayB);
