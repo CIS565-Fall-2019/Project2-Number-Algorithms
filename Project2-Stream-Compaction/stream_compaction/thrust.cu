266ea69eae7dfdf4a76efd3a8d70bb21796c4d70
Dewang Sultania <dewangsultania@gmail.com>
2019-09-13 20:27:22 -0400

    Thrust Implementation

--- Project2-Stream-Compaction/stream_compaction/thrust.cu
+++ Project2-Stream-Compaction/stream_compaction/thrust.cu
@@ -22,6 +22,12 @@ namespace StreamCompaction {
             // TODO use `thrust::exclusive_scan`
             // example: for device_vectors dv_in and dv_out:
             // thrust::exclusive_scan(dv_in.begin(), dv_in.end(), dv_out.begin());
+			thrust::host_vector<int>host_in(idata, idata + n);
+			thrust::device_vector<int>dev_in(n);
+			thrust::device_vector<int>dev_out(n);
+			thrust::copy(host_in.begin(), host_in.end(), dev_in.begin());
+			thrust::exclusive_scan(dev_in.begin(), dev_in.end(), dev_out.begin());
+			thrust::copy(dev_out.begin(), dev_out.end(), odata);
             timer().endGpuTimer();
         }
     }
