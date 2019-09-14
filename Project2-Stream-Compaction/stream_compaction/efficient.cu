1a18e1357d0e7f0c987e0a1ccb6965a72b336e15
SOMANSHU AGARWAL <somanshu@park.key.seas.upenn.edu>
2019-09-14 02:42:27 -0400

    Changes for Part2A

--- Project2-Stream-Compaction/stream_compaction/efficient.cu
+++ Project2-Stream-Compaction/stream_compaction/efficient.cu
@@ -3,6 +3,8 @@
 #include "common.h"
 #include "efficient.h"
 
+# define blockSize 32
+
 namespace StreamCompaction {
     namespace Efficient {
         using StreamCompaction::Common::PerformanceTimer;
@@ -12,13 +14,109 @@ namespace StreamCompaction {
             return timer;
         }
 
+		void printArray(int n, int *a, bool abridged = false) {
+			printf("    [ ");
+			for (int i = 0; i < n; i++) {
+				if (abridged && i + 2 == 15 && n > 16) {
+					i = n - 2;
+					printf("... ");
+				}
+				printf("%3d ", a[i]);
+			}
+			printf("]\n");
+		}
+
+		int *dev_arr1;
+		int *dev_arr2;
+		int *dev_bools;
+		int *dev_indices;
+		int *dev_odata;
+
+		__global__ void kernUpSweep(int n, int valPower2D, int *data) {
+			int index = threadIdx.x + (blockIdx.x * blockDim.x);
+			if (index >= n)
+				return;
+
+			if (index % (2 * valPower2D) == 0 && (index + (2 * valPower2D) - 1 < n) && (index + valPower2D - 1 < n)) {
+				data[index + (2 * valPower2D) - 1] += data[index + valPower2D - 1];
+			}
+		}
+
+		__global__ void kernZeroPadding(int n, int N, int *data) {
+			int index = threadIdx.x + (blockIdx.x * blockDim.x);
+			if (index >= n && index < N)
+				data[index] = 0;
+			else
+				return;
+		}
+		__global__ void kernLastElement(int n, int *data) {
+			int index = threadIdx.x + (blockIdx.x * blockDim.x);
+			if (index == n - 1)
+				data[index] = 0;
+			else
+				return;
+		}
+		__global__ void kernDownSweep(int n, int valPower2D, int *data) {
+			int index = threadIdx.x + (blockIdx.x * blockDim.x);
+			if (index >= n)
+				return;
+
+			if ((index % (2*valPower2D)== 0) && (index + (2 * valPower2D) - 1 < n) && (index + valPower2D - 1 < n)) {
+				int temp = data[index + valPower2D - 1];
+				data[index + valPower2D - 1] = data[index + (2 * valPower2D) - 1];
+				data[index + (2 * valPower2D) - 1] += temp;
+			}
+		}
         /**
          * Performs prefix-sum (aka scan) on idata, storing the result into odata.
          */
         void scan(int n, int *odata, const int *idata) {
-            timer().startGpuTimer();
-            // TODO
-            timer().endGpuTimer();
+
+			bool stopTimer = false;
+			try {
+				timer().startGpuTimer();
+			}
+			catch (const std::runtime_error& exception) {
+				stopTimer = true;
+			}
+
+			int diff = (1 << ilog2ceil(n)) - n;
+			int N = n + diff;
+
+			cudaMalloc((void**)&dev_arr1, N * sizeof(int));
+			checkCUDAErrorFn("Malloc idata into arr1 failed");
+
+			cudaMemcpy(dev_arr1, idata, sizeof(int) * n, cudaMemcpyHostToDevice);
+			checkCUDAErrorFn("Copying idata to arr1 failed");
+			
+			dim3 fullBlocksPerGrid((N + blockSize - 1) / blockSize);
+
+			if (diff) {
+				kernZeroPadding << <fullBlocksPerGrid, blockSize >>> (n,N,dev_arr1);
+			}
+
+			for (int d = 0; d <= ilog2ceil(n) - 1; d++) {
+				int valPower2D = 1 << d;
+				kernUpSweep << <fullBlocksPerGrid, blockSize >> > (N,valPower2D,dev_arr1);
+				checkCUDAErrorFn("Kernel Up Sweep Failed");
+			}
+
+			kernLastElement << <fullBlocksPerGrid, blockSize >> > (N, dev_arr1);
+
+			for (int d = ilog2ceil(n) - 1; d >= 0; d--) {
+				int valPower2D = 1 << d;
+				kernDownSweep << <fullBlocksPerGrid, blockSize >> > (N, valPower2D, dev_arr1);
+				checkCUDAErrorFn("Kernel Down Sweep Failed");
+			}
+
+			cudaMemcpy(odata, dev_arr1, sizeof(int) * n, cudaMemcpyDeviceToHost);
+			checkCUDAErrorFn("Copying back to Host failed");
+	
+			cudaFree(dev_arr1);
+
+			if(!stopTimer)
+				timer().endGpuTimer();
+
         }
 
         /**
@@ -32,9 +130,57 @@ namespace StreamCompaction {
          */
         int compact(int n, int *odata, const int *idata) {
             timer().startGpuTimer();
-            // TODO
+
+			dim3 fullBlocksPerGrid((n + blockSize - 1) / blockSize);
+
+			cudaMalloc((void**)&dev_arr2, n * sizeof(int));
+			checkCUDAErrorFn("Malloc idata into arr2 failed");
+
+			cudaMalloc((void**)&dev_bools, n * sizeof(int));
+			checkCUDAErrorFn("Malloc idata into arr3 failed");
+
+			cudaMalloc((void**)&dev_indices, n * sizeof(int));
+			checkCUDAErrorFn("Malloc idata into indices failed");
+
+			cudaMalloc((void**)&dev_odata, n * sizeof(int));
+			checkCUDAErrorFn("Malloc idata into odata failed");
+
+			cudaMemcpy(dev_arr2, idata, sizeof(int) * n, cudaMemcpyHostToDevice);
+			checkCUDAErrorFn("Copying idata to arr2 failed");
+
+			Common::kernMapToBoolean << <fullBlocksPerGrid, blockSize >> > (n,dev_bools,dev_arr2);
+			checkCUDAErrorFn("Kernel Map indicator failed");
+
+			int *indices = new int[n];
+			int *bools = new int[n];
+
+			cudaMemcpy(bools, dev_bools, sizeof(int) * n, cudaMemcpyDeviceToHost);
+			checkCUDAErrorFn("Copying bools to host failed");
+		
+			scan(n, indices, bools);
+
+			cudaMemcpy(dev_indices, indices, sizeof(int) * n, cudaMemcpyHostToDevice);
+			checkCUDAErrorFn("Copying indices to device failed");
+
+			Common::kernScatter << <fullBlocksPerGrid, blockSize >> > (n,dev_odata,dev_arr2,dev_bools,dev_indices);
+			checkCUDAErrorFn("Kernel Scatter failed");
+
+			int length = indices[n - 1];
+
+			if (idata[n - 1])
+				length += 1;
+
+			//printf("Length is %d \n", length);
+			cudaMemcpy(odata, dev_odata, sizeof(int) * length, cudaMemcpyDeviceToHost);
+			checkCUDAErrorFn("Copying back to the host failed");
+
+			cudaFree(dev_arr2);
+			cudaFree(dev_bools);
+			cudaFree(dev_indices);
+			cudaFree(dev_odata);
+
             timer().endGpuTimer();
-            return -1;
+            return length;
         }
     }
 }
--- Project2-Stream-Compaction/stream_compaction/naive.cu
+++ Project2-Stream-Compaction/stream_compaction/naive.cu
@@ -3,6 +3,8 @@
 #include "common.h"
 #include "naive.h"
 
+# define blockSize 32
+
 namespace StreamCompaction {
     namespace Naive {
         using StreamCompaction::Common::PerformanceTimer;
@@ -11,14 +13,69 @@ namespace StreamCompaction {
             static PerformanceTimer timer;
             return timer;
         }
-        // TODO: __global__
+
+		int *dev_arr1;
+		int *dev_arr2;
+
+		__global__ void kernScan(int n,int pos, int *arr2, const int *arr1) {
+			int index = threadIdx.x + (blockIdx.x * blockDim.x);
+			if (index >= n)
+				return;
+			if (index >= pos)
+				arr2[index] = arr1[index - pos] + arr1[index];
+
+		}
+
+		__global__ void kernShiftRight(int n, int *odata, int *idata) {
+			int index = threadIdx.x + (blockIdx.x * blockDim.x);
+			if (index >= n)
+				return;
+
+			if (index == 0) {
+				odata[index] = 0;
+				return;
+			}
+			odata[index] = idata[index-1];
+		}
 
         /**
          * Performs prefix-sum (aka scan) on idata, storing the result into odata.
          */
         void scan(int n, int *odata, const int *idata) {
             timer().startGpuTimer();
-            // TODO
+			dim3 fullBlocksPerGrid((n + blockSize - 1) / blockSize);
+			
+			cudaMalloc((void**)&dev_arr1, n * sizeof(int));
+			checkCUDAErrorFn("Malloc idata into arr1 failed");
+
+			cudaMalloc((void**)&dev_arr2, n * sizeof(int));
+			checkCUDAErrorFn("Malloc odata into arr2 failed");
+
+			cudaMemcpy(dev_arr1, idata, sizeof(int) * n, cudaMemcpyHostToDevice);
+			checkCUDAErrorFn("Copying idata to arr1 failed");
+
+			cudaMemcpy(dev_arr2, idata, sizeof(int) * n, cudaMemcpyHostToDevice);
+			checkCUDAErrorFn("Memory copy idata to arr2 failed");
+
+			for (int d = 1; d <= ilog2ceil(n); d++) {
+				int pos = 1 << (d - 1);
+
+				kernScan << <fullBlocksPerGrid, blockSize >> > (n,pos, dev_arr2, dev_arr1);
+				checkCUDAErrorFn("Kernel Scan failed");
+				
+				cudaMemcpy(dev_arr1, dev_arr2, sizeof(int) * n, cudaMemcpyDeviceToHost);
+				checkCUDAErrorFn("Memory copy from arr2 to arr1 failed");
+			}
+			
+			kernShiftRight << <fullBlocksPerGrid, blockSize >> > (n, dev_arr2, dev_arr1);
+			checkCUDAErrorFn("Kernel Scan failed");
+
+			cudaMemcpy(odata, dev_arr2, sizeof(int) * n, cudaMemcpyDeviceToHost);
+			checkCUDAErrorFn("Copying back to Host failed");
+
+			cudaFree(dev_arr1);
+			cudaFree(dev_arr2);
+
             timer().endGpuTimer();
         }
     }
