d6857af4ef6e787bbc489f64f69964b89efbdfec
DISHA JINDAL <djjindal@park.key.seas.upenn.edu>
2019-09-17 16:19:35 -0400

    Outputs and Original stream compaction main

--- Project2-Stream-Compaction/stream_compaction/efficient.h
+++ Project2-Stream-Compaction/stream_compaction/efficient.h
@@ -6,10 +6,10 @@ namespace StreamCompaction {
     namespace Efficient {
         StreamCompaction::Common::PerformanceTimer& timer();
 
-        void scanEfficient(int n, int *odata, const int *idata, int blockSize);
+        void scanEfficient(int n, int *odata, const int *idata, int blockSize = 128);
 
-		void scan(int n, int *odata, const int *idata, int blockSize);
+		void scan(int n, int *odata, const int *idata, int blockSize = 128);
 
-        int compact(int n, int *odata, const int *idata, bool efficient, int blockSize);
+        int compact(int n, int *odata, const int *idata, bool efficient = true, int blockSize = 128);
     }
 }
